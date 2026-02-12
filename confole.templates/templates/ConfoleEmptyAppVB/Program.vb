db27a65245966e47c32c13dfb216bb4fbef6eac7
Luca Pollicino <lucapollicino05@outlook.it>
2026-02-12 15:21:17 +0100

    Progetti vuoti 🙀

---
+++ confole.templates/templates/ConfoleEmptyAppVB/Program.vb
@@ -0,0 +1,20 @@
+' Program.vb ** ConfoleApp
+'   Confole: https://github.com/reallukee/confole/
+
+Imports System
+
+Imports Reallukee.Confole.Sharp
+
+Namespace ConfoleApp
+
+    Friend Module Program
+
+        Sub Main(args As String())
+            Console.WriteLine("Hello, World from ConfoleEmptyApp!")
+
+            Console.ReadKey(True)
+        End Sub
+
+    End Module
+
+End Namespace
