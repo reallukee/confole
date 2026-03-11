63ebdbdf97b1754eb0d4360833ec690b65e8e6f9
Luca Pollicino <lucapollicino05@outlook.it>
2026-03-11 20:50:39 +0100

    Altri esempi

---
+++ examples/ConfoleAppSharp/Program.cs
@@ -0,0 +1,28 @@
+﻿// Program.cs ** ConfoleAppSharp Example
+//   Confole: https://github.com/reallukee/confole/
+
+using System;
+
+using Reallukee.Confole.Sharp;
+
+namespace Reallukee.Confole.Examples;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Rules.DoTitle("ConfoleApp");
+
+        Formats formats =
+            Formats.Init()
+                   .Italic(true)
+                   .ForegroundColor(new RGBColor(255, 0, 0))
+                   .BackgroundColor(new RGBColor(0, 0, 255));
+
+        formats.ApplyAll("Hello, World from ConfoleApp!", true);
+
+        Console.ReadKey(true);
+
+        Formats.Reset("");
+    }
+}
